1cfb26eca434979d9340907213610ccdca63a173
Nguyen Nhieu Thinh <nguyennhieuthinh@gmail.com>
2023-07-24 11:26:56 +0700

    add bootstrap 4

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+const morgan = require('morgan')
+const path = require('path')
+
+const app = express()
+const port = 3000
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname,'resource/views'));
+
+app.use(morgan('combined'))
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
